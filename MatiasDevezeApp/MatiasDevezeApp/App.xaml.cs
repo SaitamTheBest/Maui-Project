5c4f92006299a8f61ddfdb822fa741880913d7d8
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-03-13 16:05:57 +0100

    Initial project

---
+++ MatiasDevezeApp/MatiasDevezeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MatiasDevezeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
